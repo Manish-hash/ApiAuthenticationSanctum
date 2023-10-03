3c247e5833a9c3a1ddf23e5606998296de29f6f3
Manish <manishtimalsina51@gmail.com>
2023-10-03 15:01:42 +0545

    make route ,contoller

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -12,6 +13,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Manish Timalsina',
+            'email' => 'manish@gmail.com',
+            'password' => Hash::make('password')
+      ]);
     }
 }
